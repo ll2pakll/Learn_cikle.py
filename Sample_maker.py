6af29a623fd5982a8495ff50ea367f81354d80bb
ll2pakll <ll2pakll@gmail.com>
2021-12-25 01:12:02 +0100

    Исправил ошибки в sample_maker

--- Sample_maker.py
+++ Sample_maker.py
@@ -7,17 +7,17 @@ class Sample_maker:
     def __init__(self, img_path, pad_factor=1, resolution=512, shift_centr_factor=(1, 1), show_points=False):
         self.image = cv2.imread(img_path)
         self.points = DFLIMG.DFLJPG.load(img_path).get_dict()['target'] # потом заменить на predict
-        if show_points:
-            self.draw_pts()
         self.change_image = self.image.copy()
         self.change_points = self.points.copy()
         self.pad_factor = pad_factor
         self.resolution = resolution
-        self.shift_centr_factor = shift_centr_factor
+        self.shift_centr_factor = list(shift_centr_factor)
         self.height = int((self.change_points[3][1] - self.change_points[0][1]))
+        self.show_points = show_points
+        print(self.change_points)
 
     def all_transformations(self):
-        self.centr_and_rotate().crop().resize()
+        self.rotate().centr().crop().resize()
         return self
 
     def make_angle(self):
@@ -25,12 +25,25 @@ class Sample_maker:
         a = self.points[3][0] - self.points[0][0]
         angle = math.degrees(math.atan(a / b))
         return angle
+    def rotate(self):
+        transform = A.Compose([
+            A.Affine(rotate=self.make_angle(), p=1),
+        ],
+            keypoint_params=A.KeypointParams(format='xy', remove_invisible=None),
+            p=1)
 
-    def centr_and_rotate(self):
+        transformed = transform(image=self.change_image, keypoints=self.change_points)
+        self.change_image, self.change_points = transformed['image'], transformed['keypoints']
+        return self
+
+    def centr(self):
         (y, x, d) = self.change_image.shape
 
-        centr_y = y//2 - int(self.height*self.shift_centr_factor[1] - self.height)
-        centr_x = x//2 - int(self.height*self.shift_centr_factor[0] - self.height)
+        if self.change_points[1][0] > self.change_points[2][0]:
+            centr_x = x // 2 + int(self.height * self.shift_centr_factor[0] - self.height)
+        else:
+            centr_x = x//2 - int(self.height * self.shift_centr_factor[0] - self.height)
+        centr_y = y // 2 - int(self.height * self.shift_centr_factor[1] - self.height)
 
         if centr_y < 0:
             y_centr = 0
@@ -51,7 +64,6 @@ class Sample_maker:
         shift_y = img_centr[0] - centr_point[1]
         shift_x = img_centr[1] - centr_point[0]
         transform = A.Compose([
-            A.Affine(rotate=self.make_angle(), p=1),
             A.Affine(translate_px={'x':shift_x, 'y':shift_y}, p=1)
         ],
         keypoint_params=A.KeypointParams(format='xy', remove_invisible=None),
@@ -98,22 +110,23 @@ class Sample_maker:
         return self
 
     def draw_pts(self):
-        overlay = self.image.copy()
+        overlay = self.change_image.copy()
         colors = [[0, 255, 255], [255, 0, 0], [0, 0, 255], [0, 255, 0]]
         marker_radius = 10
         alpha = 0.8
-        for i, point in enumerate(self.points):
-            if point.any():
-                overlay = cv2.circle(overlay, tuple(point), marker_radius, tuple(colors[i]), thickness=-1)
-        self.image = cv2.addWeighted(overlay, alpha, self.image, 1 - alpha, 0)
+        for i, point in enumerate(self.change_points):
+            overlay = cv2.circle(overlay, (int(point[0]), int(point[1])), marker_radius, tuple(colors[i]), thickness=-1)
+        self.change_image = cv2.addWeighted(overlay, alpha, self.change_image, 1 - alpha, 0)
 
     def image_return(self):
+        if self.show_points:
+            self.draw_pts()
         return self.change_image
 
     def points_return(self):
         return self.change_points
 
-def viewImage(image, name_of_window):
+def viewImage(image, name_of_window = 'Window'):
     cv2.namedWindow(name_of_window)
     cv2.imshow(name_of_window, image)
     cv2.waitKey(0)
@@ -121,7 +134,7 @@ def viewImage(image, name_of_window):
 
 
 name_window = 'Window'
-img_path ='d:\Work Area\Xseg_exstract\\frames\\0001.jpg'
+img_path ='d:\Work Area\Xseg_exstract\\frames\\0127.jpg'
 path_write = 'd:\Work Area\Xseg_exstract\\img_test.jpg'
 
 sempler = Sample_maker(img_path, pad_factor=1.1, resolution=512, shift_centr_factor=(1.1, 1), show_points=True)
@@ -133,4 +146,4 @@ image_cheng = sempler.image_return()
 print(sempler.points_return())
 cv2.imwrite(path_write, image_cheng)
 
-viewImage(image_cheng, name_window)
+viewImage(image_cheng)
